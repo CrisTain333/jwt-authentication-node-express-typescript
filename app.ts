e6265f05faf3598dbc9537771261329a44e754fd
Sukanta Das <arnobh84@gmail.com>
2023-07-25 16:30:50 +0600

    completed

---
+++ app.ts
@@ -0,0 +1,172 @@
+import express from "express";
+import { Application } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { User } from "./models/user";
+// Create the express app and  import the type of app from express;
+const app: Application = express();
+
+// Cors
+app.use(cors());
+//configure env;
+dotenv.config();
+// Parser
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+// Declare The PORT Like This
+const PORT: number = 8000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome To JWT Authentication </h1>");
+});
+
+app.post("/auth/register", async (req, res) => {
+  try {
+    // ** Get The User Data From Body ;
+    const user = req.body;
+
+    // ** destructure the information from user;
+    const { name, email, password } = user;
+
+    // ** Check the email all ready exist  in database or not ;
+    // ** Import the user model from "./models/user";
+
+    const isEmailAllReadyExist = await User.findOne({
+      email: email,
+    });
+
+    // ** Add a condition if the user exist we will send the response as email all ready exist
+    if (isEmailAllReadyExist) {
+      res.status(400).json({
+        status: 400,
+        message: "Email all ready in use",
+      });
+      return;
+    }
+
+    // ** if not create a new user ;
+    // !! Don't save the password as plain text in db . I am saving just for demonstration.
+    // ** You can use bcrypt to hash the plain password.
+
+    // now create the user;
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    // Send the newUser as  response;
+    res.status(200).json({
+      status: 201,
+      success: true,
+      message: " User created Successfully",
+      user: newUser,
+    });
+  } catch (error: any) {
+    // console the error to debug
+    console.log(error);
+
+    // Send the error message to the client
+    res.status(400).json({
+      status: 400,
+      message: error.message.toString(),
+    });
+  }
+});
+
+app.post("/auth/login", async (req, res) => {
+  try {
+    // ** Get The User Data From Body ;
+    const user = req.body;
+
+    // ** destructure the information from user;
+    const { email, password } = user;
+
+    // ** Check the (email/user) exist  in database or not ;
+    const isUserExist = await User.findOne({
+      email: email,
+    });
+
+    // ** if there is not any user we will send user not found;
+    if (!isUserExist) {
+      res.status(404).json({
+        status: 404,
+        success: false,
+        message: "User not found",
+      });
+      return;
+    }
+
+    // ** if the (user) exist  in database we will check the password is valid or not ;
+    // **  compare the password in db and the password sended in the request body
+
+    const isPasswordMatched =
+      isUserExist?.password === password;
+
+    // ** if not matched send response that wrong password;
+
+    if (!isPasswordMatched) {
+      res.status(400).json({
+        status: 400,
+        success: false,
+        message: "wrong password",
+      });
+      return;
+    }
+
+    // ** if the email and password is valid create a token
+
+    /*
+    To create a token JsonWebToken (JWT) receive's 3 parameter
+    1. Payload -  This contains the claims or data you want to include in the token.
+    2. Secret Key - A secure key known only to the server used for signing the token.
+    3. expiration -  Additional settings like token expiration or algorithm selection.
+    */
+
+    // !! Don't Provide the secret openly, keep it in the .env file. I am Keeping Open just for demonstration
+
+    // ** This is our JWT Token
+    const token = jwt.sign(
+      { _id: isUserExist?._id, email: isUserExist?.email },
+      "YOUR_SECRET",
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // send the response
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "login success",
+      token: token,
+    });
+  } catch (error: any) {
+    // Send the error message to the client
+    res.status(400).json({
+      status: 400,
+      message: error.message.toString(),
+    });
+  }
+});
+
+// Listen the server
+app.listen(PORT, async () => {
+  console.log(`🗄️  Server Fire on http:localhost//${PORT}`);
+
+  // Connect To The Database
+  try {
+    await mongoose.connect(
+      process.env.DATABASE_URL as string
+    );
+    console.log("🛢️  Connected To Database");
+  } catch (error) {
+    console.log("⚠️ Error to connect Database");
+  }
+});
---
+++ models/user.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("Users", UserSchema);
